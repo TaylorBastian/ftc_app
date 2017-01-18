41473067a4df8d9ad909ffdce026bc96fded79c2
Taylor Bastian <taylorjb16@gmail.com>
2017-01-18 15:08:00 -0700

    Added comments and fixed spinner motor

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RMHSDrive.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RMHSDrive.java
@@ -11,17 +11,25 @@ public class RMHSDrive extends LinearOpMode {
     DcMotor spinnermotor;
     DcMotor shootermotor;
     Servo pusher;
+
+
     @Override
     public void runOpMode() throws InterruptedException {
+
+        //hardware map stuff
         leftmotor=hardwareMap.dcMotor.get("l");
         rightmotor=hardwareMap.dcMotor.get("r");
         spinnermotor=hardwareMap.dcMotor.get("s");
         shootermotor=hardwareMap.dcMotor.get("ss");
         pusher=hardwareMap.servo.get("p");
-
-        rightmotor.setDirection(DcMotor.Direction.REVERSE);
+        
+        //set servo start position
         pusher.setPosition(0.5);
+
+        //Wait for play button to start
         waitForStart();
+
+        //Op mode loop
         while(opModeIsActive()){
 
 
@@ -45,9 +53,16 @@ public class RMHSDrive extends LinearOpMode {
             if(gamepad1.a){
                 shootermotor.setPower(-1.0);
             }
+            else{
+                shootermotor.setPower(0);
+            }
+
+            //Push ball
             if(gamepad1.dpad_right) {
                 pusher.setPosition(0);
             }
+
+            //Reset servo position
             if(gamepad1.dpad_left) {
                 pusher.setPosition(0.5);
             }
